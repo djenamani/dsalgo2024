bd9365e69226ccf0ccc92e7d5b99af093599f438
djenamani <dibyendujenamani@gmail.com>
2024-10-26 16:45:32 +0530

    Second Commit: Palindrome problem basic solution - change 1

--- src/main/java/com/dj/ds/algo/Palindrome.java
+++ src/main/java/com/dj/ds/algo/Palindrome.java
@@ -4,21 +4,18 @@ package com.dj.ds.algo;
  * Class file to check whther an input array or string is palindrome or not.
  * Palindrome Definition: Is the sequence which can be read same forward and backward.
  * Ex: 'madam', 'racecar' are palindrome but 'xyzabc' is not palindrome
- *
  * Solution: Use two pointer pattern to verify and return a boolean value
  *
  * @author Dibyendu Jenamani
- * @Date   26-OCT-2024
  */
 public class Palindrome {
 
     static boolean isPalindrome(String input){
 
-        char[] chars = input.toCharArray();
         int i = 0, j = input.length() - 1;
         // traverse elements in the array till they meet
         while (i < j){
-            if(chars[i] == chars[j]){
+            if(input.charAt(i) == input.charAt(j)){
                 i++; j--;
             }
             else return false;
@@ -27,6 +24,7 @@ public class Palindrome {
     }
 
     public static void main(String[] args) {
-        System.out.println(isPalindrome("racecar"));
+        System.out.println("abcxzy is palindrome: " + isPalindrome("abcxzy"));
+        System.out.println("madam is palindrome: "+ isPalindrome("madam"));
     }
 }
